23735036ae0f417d611a7485481ef40e390a8969
Ramakanth <kanth465@gmail.com>
2018-04-10 14:45:05 +0530

    Added singleton class

---
+++ GitTrailConsole/GitTrailConsole/singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTrailConsole
+{
+    public sealed class singleton
+    {
+        private static int counter = 0;
+        private static singleton Instance = null;
+        public static singleton GetInstance
+        {
+            get
+            {
+                if (Instance == null)
+                    Instance = new singleton();
+                return Instance;
+            }
+        }
+        private singleton()
+        {
+            counter++;
+            Console.WriteLine("counter value " + counter);
+        }
+        public void printdetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
